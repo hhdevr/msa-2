1e8369831d0de42d79f12ba23759463f4840ca43
Dmitry Chaykin <hhdevr@gmail.com>
2026-03-29 21:07:26 +0600

    init commit

---
+++ src/main/java/com/chaykin/msa2/Msa2Application.java
@@ -0,0 +1,13 @@
+package com.chaykin.msa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa2Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Msa2Application.class, args);
+    }
+
+}
