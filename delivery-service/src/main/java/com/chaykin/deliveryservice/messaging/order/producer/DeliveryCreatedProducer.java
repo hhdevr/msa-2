025518f06186ab4cfded3cc752cfa9cc8834d597
Dmitry Chaykin <hhdevr@gmail.com>
2026-05-13 22:09:05 +0600

    11. Idempotent consumer

--- delivery-service/src/main/java/com/chaykin/deliveryservice/messaging/order/producer/DeliveryCreatedProducer.java
+++ delivery-service/src/main/java/com/chaykin/deliveryservice/messaging/order/producer/DeliveryCreatedProducer.java
@@ -4,24 +4,43 @@ import com.chaykin.common.model.messaging.DeliveryCreatedMessage;
 import com.chaykin.deliveryservice.messaging.order.config.properties.KafkaOrderServiceProperties;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class DeliveryCreatedProducer {
 
+    public static final String IDEMPOTENCY_KEY_HEADER = "X-Idempotency-Key";
+
     private final KafkaTemplate<String, DeliveryCreatedMessage> kafkaTemplate;
     private final KafkaOrderServiceProperties properties;
 
     public void send(DeliveryCreatedMessage message) {
-        log.info("Sending delivery created message to Kafka topic={} for order {}, delivery {}",
-                 properties.deliveryCreatedTopic(),
+        String topic = properties.deliveryCreatedTopic();
+        String key = message.orderRefId().toString();
+        String idempotencyKey = UUID.randomUUID().toString();
+
+        log.info("Sending delivery created message to topic={} order={} delivery={} idempotencyKey={}",
+                 topic,
                  message.orderRefId(),
-                 message.deliveryGuid());
-        kafkaTemplate.send(properties.deliveryCreatedTopic(),
-                           message.orderRefId().toString(),
-                           message);
+                 message.deliveryGuid(),
+                 idempotencyKey);
+
+        RecordHeader header = new RecordHeader(IDEMPOTENCY_KEY_HEADER, idempotencyKey.getBytes());
+        ProducerRecord<String, DeliveryCreatedMessage> record = new ProducerRecord<>(topic,
+                                                                                     null,
+                                                                                     null,
+                                                                                     key,
+                                                                                     message,
+                                                                                     List.of(header));
+
+        kafkaTemplate.send(record);
     }
 }
--- order-service/src/main/java/com/chaykin/orderservice/messaging/delivery/listener/DeliveryCreatedListener.java
+++ order-service/src/main/java/com/chaykin/orderservice/messaging/delivery/listener/DeliveryCreatedListener.java
@@ -1,31 +1,39 @@
 package com.chaykin.orderservice.messaging.delivery.listener;
 
 import com.chaykin.common.model.messaging.DeliveryCreatedMessage;
+import com.chaykin.orderservice.messaging.common.listener.IdempotentKafkaListener;
+import com.chaykin.orderservice.service.AsyncMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.json.JsonMapper;
 
 @Slf4j
 @Component
-public class DeliveryCreatedListener {
+public class DeliveryCreatedListener extends IdempotentKafkaListener<DeliveryCreatedMessage> {
+
+    public DeliveryCreatedListener(AsyncMessageService asyncMessageService, JsonMapper jsonMapper) {
+        super(asyncMessageService, jsonMapper);
+    }
 
     @KafkaListener(topics = "${kafka.service.delivery.delivery-created-topic}",
                    groupId = "${spring.kafka.consumer.group-id}",
                    properties = {
                            "spring.json.value.default.type=com.chaykin.common.model.messaging.DeliveryCreatedMessage"
                    })
+    @Override
     public void consume(ConsumerRecord<String, DeliveryCreatedMessage> consumerRecord,
                         DeliveryCreatedMessage message,
-                        Acknowledgment ack) {
-        log.info("Received delivery created message: orderRefId={}, deliveryGuid={} (key={}, partition={}, offset={})",
-                 message.orderRefId(),
-                 message.deliveryGuid(),
-                 consumerRecord.key(),
-                 consumerRecord.partition(),
-                 consumerRecord.offset());
+                        Acknowledgment acknowledgment) {
+        super.consume(consumerRecord, message, acknowledgment);
+    }
 
-        ack.acknowledge();
+    @Override
+    public void processConsumedMessage(DeliveryCreatedMessage message) {
+        log.info("Consumed delivery.created event: orderRefId={} deliveryGuid={}",
+                 message.orderRefId(),
+                 message.deliveryGuid());
     }
 }
